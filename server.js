21f54aa3818f9dec34b4ce1f7a93d28a7cd5eb78
Mo'men Maged <momen99223@gmail.com>
2022-02-21 19:31:53 +0200

    added sqlite database and some preliminary endpoints

--- server.js
+++ server.js
@@ -1,53 +1,98 @@
-class setup_server{
-    constructor(directory_path , db_path , port_num){
-            const express = require("express"); //require express package
-            const app = express();
-            const path = require(directory_path);
-            const file_system = require("fs");
-            // constants
-            const DB_PATH = path.resolve(db_path);
-            const PORT = process.env.PORT || port_num;
-            app.listen(PORT, () => console.log("Listening on port", PORT));
-        }
-}
+var express = require("express");
+var app = express();
+var db = require("./database.js");
+// Server port
+var HTTP_PORT = 8000;
+// Start server
+app.listen(HTTP_PORT, () => {
+  console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
+});
+// Root endpoint
+app.get("/", (req, res, next) => {
+  res.json({ message: "Ok" });
+});
 
-class server_controller extends setup_server{
-    constructor (){
-        super();
-    };
-    
-    Get(){
-        app.get("/", async (req, res) => {
-        fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
-            if (err) return console.log("Error in reading from db");
-            let values = JSON.parse(jsonString);
-            res.status(200).json({
-            totalValues: values.length,
-            values,
-            });
-        });
-        });
+// Insert here other API endpoints
+
+// Default response for any other request
+app.use(function (req, res) {
+  res.status(404);
+});
+
+app.post("/temperature/", (req, res, next) => {
+  var errors = [];
+  if (!req.body.reading) {
+    errors.push("No reading sent");
+  }
+  var data = {
+    reading: req.body.reading,
+    time: new Date(),
+  };
+  var sql = "INSERT INTO temperature (reading, time) VALUES (?,?)";
+  var params = [data.reading, data.time];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
     }
-    post(){
-        app.post("/", async (req, res) => {
-        fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
-            if (err) return console.log("Error in reading from db");
-            let body = req.body;
-            let valuesArr = JSON.parse(jsonString);
-            let obj = {
-                temperature: body.temperature,
-                humidity: body.humidity,
-                timestamp: new Date(),
-            };
-            valuesArr.push(obj);
-            fs.writeFile(DB_PATH, JSON.stringify(valuesArr), (err) => {
-                if (err) return console.log("Error in updating db");
-                res.status(200).json({
-                message: "Values saved",
-                value: valuesArr[valuesArr.length - 1],
-                });
-            });
-            });
-        });
+    res.json({
+      message: "success",
+      data: data,
+      id: this.lastID,
+    });
+  });
+});
+
+app.post("/pressure/", (req, res, next) => {
+  var errors = [];
+  if (!req.body.reading) {
+    errors.push("No reading sent");
+  }
+  var data = {
+    reading: req.body.reading,
+    time: new Date(),
+  };
+  var sql = "INSERT INTO pressure (reading, time) VALUES (?,?)";
+  var params = [data.reading, data.time];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
     }
-}
+    res.json({
+      message: "success",
+      data: data,
+      id: this.lastID,
+    });
+  });
+});
+
+app.get("/temperature/", (req, res, next) => {
+  var sql = "select * from tempertaure";
+  var params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.mesasge });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+app.get("/pressure/", (req, res, next) => {
+  var sql = "select * from pressure";
+  var params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.mesasge });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
